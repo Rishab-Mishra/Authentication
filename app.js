154feee72001e76a871a780ba9ec04badec01fc3
Rishab-Mishra <rishabh.mishra.8859@gmail.com>
2023-10-17 01:04:21 +0530

    Level 2 authentication using Encryption in mongoose, and applied dotenv

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+import {} from 'dotenv/config'
 import express from "express";
 import mongoose from "mongoose";
-
+import encrypt from "mongoose-encryption";
 
 const app = express();
 const port = 3000;
@@ -9,12 +10,17 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 
+
+
 mongoose.connect("mongodb://localhost:27017/userDB");
 
-const userSchema ={
+const userSchema = new mongoose.Schema ({
     email: String,
     password: String
-};
+});
+
+
+userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields:["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
